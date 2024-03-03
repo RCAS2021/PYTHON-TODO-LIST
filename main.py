86ed41b4cb8ae9a997bd743bd8b42bd423a2c6ee
Rafael Cruz <rcas2009@outlook.com>
2024-03-03 10:29:28 -0300

    feat: implementing task creation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -21,9 +21,24 @@ class Todo(db.Model):
     def __repr__(self):
         return f"<Task {self.id}>"
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template('index.html')
+
+    if request.method == "POST":
+        task_content = request.form["content"]
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an error adding your task"
+
+    else:
+        # Looks into all database contents ordered by date created. all() returns all
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
